c9e570dd2c90de268c5f5bf7e06ee71d7add4bc3
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-10-30 17:03:13 +0300

    added day 9 classwork

---
+++ src/main/java/tasks/classwork/day9/IMyInterface.java
@@ -0,0 +1,15 @@
+package tasks.classwork.day9;
+import java.lang.FunctionalInterface;
+
+
+//@FunctionalInterface
+////public interface IMyInterface {
+////    double doSomething();
+////}
+
+public interface IMyInterface {
+//    void doSomething(String string);
+
+    ClassX doSomething(String string);
+
+}

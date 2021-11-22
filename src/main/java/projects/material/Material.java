bfaee9352250a8442d6b3cbdb56c60c3d7579741
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-11-22 13:21:44 +0300

    added Material Enum

--- src/main/java/projects/material/Material.java
+++ src/main/java/projects/material/Material.java
@@ -2,6 +2,29 @@ package projects.material;
 
 public enum Material {
 
-    GLASS,PLASTIC,METAL;
+    GLASS, PLASTIC, METAL;
+
+    private double thermalConductivity;
+    private String color;
+    private double density;
+
+    private void Glass() {
+        this.thermalConductivity = 0.017;
+        this.color = "green";
+        this.density = 2200;
+    }
+
+    private void Plastic() {
+        this.thermalConductivity = 0.2;
+        this.color = "white";
+        this.density = 1800;
+    }
+
+    private void Metal() {
+        this.thermalConductivity = 58.0;
+        this.color = "silver";
+        this.density = 7800;
+    }
 
 }
+

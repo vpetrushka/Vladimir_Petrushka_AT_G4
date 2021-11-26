a82be9ccf5986db033e2641d892c58f6d29affe4
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-11-26 08:24:43 +0300

    moved Classes

---
+++ src/main/java/tasks/homework/unit_tests_dummy/Calculator.java
@@ -0,0 +1,21 @@
+package tasks.homework.unit_tests_dummy;
+
+public class Calculator {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+}
---
+++ src/main/java/tasks/homework/unit_tests_dummy/Rectangle.java
@@ -0,0 +1,31 @@
+package tasks.homework.unit_tests_dummy;
+
+public class Rectangle {
+
+    double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getArea(){
+        return this.length * this.width;
+    }
+}

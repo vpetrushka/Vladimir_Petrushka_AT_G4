8aed14ad71161046358bcfbc694490db77a01c32
Vladimir Petrushka <vladimir.petrushka@gmail.com>
2021-12-02 08:13:52 +0300

    runner

--- src/test/java/project/gui/booking/DateChecker.java
+++ src/test/java/project/gui/booking/DateChecker.java
@@ -1,49 +1,39 @@
 package project.gui.booking;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class DateChecker {
 
-    public static boolean checkForDate(LocalDate date){
+    @Test
+    public static boolean checkForDate(String date) {
 
         WebDriver driver = new ChromeDriver();
-
         driver.navigate().to("https://booking.com");
         driver.findElement(By.xpath("//input[@type = 'search']")).sendKeys("Москва");
-
-
+        driver.findElement(By.xpath("//button[@data-sb-id='main']")).click();
+        driver.findElement(By.xpath(String.format("//td[@data-date = '%s']", date))).click();
+        String value = Arrays.stream(driver.findElement(By.xpath("//h1[contains(text(),'Moscow')]")).getText().split(" ")).skip(1).findFirst().get();
         driver.close();
         driver.quit();
-
-
-
-
-
-
-
-
-        return true;
-
+        return Integer.parseInt(value.replaceAll(",", "")) > 0;
     }
 
     public static void main(String[] args) {
 
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("input date in forman dd/mm/yyyy");
-//        LocalDate date = in.nextLine(DateTimeFormatter.ofPattern("dd/MM/uuuu"))
-        checkForDate(LocalDate.now());
     }
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
